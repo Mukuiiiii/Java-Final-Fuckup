cace3de662d11653f9185d4e3febc30f0cb1cce8
silvercow02 <66425747+silvercow002@users.noreply.github.com>
2026-04-26 14:36:23 +0800

    move add draw

---
+++ core/src/main/java/move/draw.java
@@ -0,0 +1,31 @@
+package move;
+
+
+import component.CardStack;
+import component.player;
+import unhappyEC.Entity;
+
+// TODO:
+//  rename the class
+public class draw {
+    public static void drawCard(Entity player, String from, String to) {
+        CardStack fromStack = player.getComponent(player.class).getStack(from).getComponent(CardStack.class);
+        CardStack toStack = player.getComponent(player.class).getStack(to).getComponent(CardStack.class);
+
+        if (fromStack.size() == 0) {
+//         pass
+//         TODO:
+//            add exeception
+        }
+        else {
+            Entity card = fromStack.Top();
+            fromStack.popFromTop();
+            toStack.insertCard(card);
+        }
+    }
+
+
+
+
+
+}
