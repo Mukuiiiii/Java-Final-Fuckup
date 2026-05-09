19d246b334df0a860ff5e8662f2087aac6183d7e
Mukuiiiii <justinweng0423@gmail.com>
2026-05-09 16:27:59 +0800

    i don't know

---
+++ core/src/main/java/component/EffectComponent.java
@@ -0,0 +1,10 @@
+package component;
+
+import unhappyEC.Component;
+import unhappyEC.Entity;
+
+public class EffectComponent extends Component {
+    public String triggerType;
+    public Predicate<Entity> condition; //I haven't write it.
+    public Runnable action;
+}
